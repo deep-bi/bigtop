2ac23c107a05e91320542523d4c22c733b4c4162
Bartosz Mikulski <bartmiki97@gmail.com>
2024-06-17 16:08:29 +0200

    Apply patch with numeric order, remove old hadoop jars

--- patches.py
+++ patches.py
@@ -1,11 +1,46 @@
 #!/usr/bin/env python3
 
+import abc
 import argparse
 from pathlib import Path
 import re
 
 PACKAGES_DIR = Path(__file__).parent / 'bigtop-packages/src/common'
 PACKAGES = {path.name: path for path in PACKAGES_DIR.iterdir() if path.is_dir()}
+
+
+class Command(abc.ABC):
+
+    @abc.abstractmethod
+    def __call__(self, args): ...
+
+    @abc.abstractmethod
+    def name(self): ...
+
+    @abc.abstractmethod
+    def configure_subparser(self, parser): ...
+
+
+class ListPatches(Command):
+
+    def __call__(self, args):
+        package_directory = PACKAGES[args.package]
+        patches = package_directory.glob('*.diff')
+        digit_pattern = re.compile(r'\d+')
+
+        def extract_number(patch: Path):
+            m = digit_pattern.search(patch.name)
+            return int(m.group()) if m else 0
+
+        patches = sorted(patches, key=extract_number)
+        for patch in patches:
+            print(patch.name)
+
+    def name(self):
+        return 'ls'
+
+    def configure_subparser(self, parser):
+        pass
 
 
 def list_patches(path: Path):
@@ -21,20 +56,26 @@ def list_patches(path: Path):
         print(patch.name)
 
 
-def package_directory(path: Path):
-    print(path.absolute())
+# def package_directory(path: Path):
+    # print(path.absolute())
 
 
-OPERATIONS = {
-    'ls': list_patches,
-    'where': package_directory
-}
+COMMANDS = [
+    ListPatches(),
+    # 'where': package_directory
+]
 
 
 def parse_args():
     ap = argparse.ArgumentParser()
     ap.add_argument('package', choices=PACKAGES.keys())
-    ap.add_argument('operation')
+
+
+    subparsers = ap.add_subparsers(required=True)
+    for command in COMMANDS:
+        subparser = subparsers.add_parser(command.name(), parents=[ap], add_help=False)
+        command.configure_subparser(subparser)
+        subparser.set_defaults(func=command)
 
     return ap.parse_args()
 
@@ -42,7 +83,7 @@ def parse_args():
 def main():
     args = parse_args()
     package = PACKAGES[args.package]
-    operation = OPERATIONS[args.operation]
+    operation = COMMANDS[args.operation]
 
     operation(package)
 
