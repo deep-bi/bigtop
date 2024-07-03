1825f6df368283310291ee045ce4b959cddb0d88
mw <michal.wisniewski@deep.bi>
2024-07-03 17:18:14 +0200

    Applying patches

--- start-rockylinux8.sh
+++ start-rockylinux8.sh
@@ -3,8 +3,8 @@
 mkdir -p cache/.gradle
 mkdir -p cache/.m2
 
-docker run --rm -u jenkins:jenkins \
-    -v `pwd`:/ws \
-    -v `pwd`/cache/.gradle:/var/lib/jenkins/.gradle \
-    -v `pwd`/cache/.m2/:/var/lib/jenkins/.m2 \
+podman run --rm -u jenkins:jenkins --userns=keep-id \
+    -v `pwd`:/ws:z \
+    -v `pwd`/cache/.gradle:/var/lib/jenkins/.gradle:z \
+    -v `pwd`/cache/.m2/:/var/lib/jenkins/.m2:z \
     -it --workdir /ws bigtop/slaves:3.2.1-rockylinux-8 bash -l
